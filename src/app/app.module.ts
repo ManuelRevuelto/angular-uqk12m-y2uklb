69e23fb1b6bf9873b59cb78939ff32c8f4e37cdb
ManuRevuelto <fp.csifc01.2122.16@gmail.com>
2022-01-20 18:00:13 +0100

    Projects info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProyectListComponent } from './proyect-list/proyect-list.component';
 import { NgFallimgModule } from 'ng-fallimg';
+import { ProjectInfoComponent } from './project-info/project-info.component';
 
+
+const appRoutes: Routes = [
+  { path: 'projects', component: ProyectListComponent },
+  { path: 'hola', component: ProjectInfoComponent },
+  { path: '',   redirectTo: '/projects', pathMatch: 'full' }
+];
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProyectListComponent },
-    ]),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     NgFallimgModule.forRoot({
       default: '/assets/PJ.png',
       post: '/assets/PJ.png'
@@ -23,7 +31,8 @@ import { NgFallimgModule } from 'ng-fallimg';
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProyectListComponent
+    ProyectListComponent,
+    ProjectInfoComponent
   ],
   bootstrap: [
     AppComponent
