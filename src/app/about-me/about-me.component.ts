517258fbfbe9be9ed8fdf5bcf803bafa9c0fea46
Manuel Revuelto <manu.revuelto1111@gmail.com>
2022-06-26 18:58:04 +0200

    diseño responsive acabado

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about-me',
-  templateUrl: './about-me.component.html',
+  selector: "app-about-me",
+  templateUrl: "./about-me.component.html",
 })
-export class AboutMeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AboutMeComponent {
+  patri!: string;
+  constructor() {
+    this.patri =
+      "https://www.esda.es/resize/245x245_crop/files/images/u709/yop66546.jpg";
+      console.log()
   }
-
 }
--- src/app/project-info/project-info.component.ts
+++ src/app/project-info/project-info.component.ts
@@ -7,27 +7,12 @@ import { Project, projects } from "../models/projects";
   templateUrl: "./project-info.component.html",
 })
 export class ProjectInfoComponent implements OnInit {
-
   project: Project | undefined;
-  projects = projects;
-  
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    // First get the project id from the current route.
-    const routeParams = this.route.snapshot.paramMap;
-    const projectLabelFromRoute = String(routeParams.get("label"));
-
-    // Find the project that correspond with the id provided in route.
-    this.project = projects.find(
-      (project) => project.label === projectLabelFromRoute
-    );
-
-    if ("loading" in HTMLImageElement.prototype) {
-      console.log("El navegador soporta `lazy-loading`...");
-    } else {
-      console.log("`lazy-loading` no soportado...");
-    }
-
+    const routeParams = this.route.snapshot.paramMap.get("label");
+    this.project = projects.find((project) => project.label === routeParams);
+    console.log(this.project);
   }
 }
